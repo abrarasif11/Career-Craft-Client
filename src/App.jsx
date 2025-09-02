9c090ede6b5f8876d6d1535b8a0232cf2ef1a6f6
abrarasif11 <faasif929@gmail.com>
2025-09-02 20:56:55 +0600

    tailwind setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
-      <h2>Welcome to career Craft Client site</h2>
+      <h2 className='text-2xl'>Welcome to career Craft Client site</h2>
     </>
   )
 }
