58baa74ed7c6dc99ff33a619782420011c75de41
abrarasif11 <faasif929@gmail.com>
2025-09-04 23:40:07 +0600

    google login auth

--- src/Context/AuthContext/AuthProvider.jsx
+++ src/Context/AuthContext/AuthProvider.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import AuthContext from './AuthContext'
-import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
 import auth from '../../firebase/firebase.init';
 const AuthProvider = ({children}) => {
     const [user, setUser] = useState(null)
@@ -19,6 +19,10 @@ const AuthProvider = ({children}) => {
     const signOutUser = () => {
         setLoading(true);
         return signOut(auth);
+    }
+
+    const signInwithGoogle = () => {
+     return signInWithPopup(auth ,) 
     }
 
     useEffect(() => {
