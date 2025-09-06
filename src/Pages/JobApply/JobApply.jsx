5faf909954d59a271b8bdab00e0c603ebfa196d0
abrarasif11 <faasif929@gmail.com>
2025-09-06 10:58:22 +0600

    find job apply value

--- src/Pages/JobApply/JobApply.jsx
+++ src/Pages/JobApply/JobApply.jsx
@@ -1,11 +1,25 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 const JobApply = () => {
+  const id = useParams()
+  console.log(id)
+
+  const handleJobApply = e => {
+    e.preventDefault();
+    const form = e.target;
+    const contact = form.contact.value;
+    const linkedin = form.linkedin.value;
+    const github = form.github.value;
+    // const resume = form.resume.value;
+    console.log(contact, linkedin, github)
+  }
   return (
     <section className="p-6 text-gray-800">
       <form
+      onSubmit={handleJobApply}
         noValidate=""
-        className="container w-full max-w-xl p-8 mx-auto space-y-6 rounded-md shadow bg-gray-50"
+        className="container w-full mt-10 mb-10 max-w-xl p-8 mx-auto space-y-6 rounded-md shadow bg-gray-50"
       >
         <p className="text-center bg-">Job Application</p>
         <h2 className="w-full text-3xl text-center font-bold leading-tight">
@@ -14,30 +28,32 @@ const JobApply = () => {
         <p className="text-center">
           Please fill in your information and send it to the employer.
         </p>
-        <div>
+        {/* <div>
           <label htmlFor="name" className="block mb-1 ml-1">
             Full Name *
           </label>
           <input
             id="name"
+            name="name"
             type="text"
             placeholder="John Kabir"
             required=""
             className="block w-full p-2 rounded focus:outline-none focus:ring focus:ring-opacity-25 focus:ring-violet-600 bg-gray-100"
           />
-        </div>
-        <div>
+        </div> */}
+        {/* <div>
           <label htmlFor="email" className="block mb-1 ml-1">
             Email *
           </label>
           <input
             id="email"
+            name="email"
             type="email"
             placeholder="john.kabir@gmail.com"
             required=""
             className="block w-full p-2 rounded focus:outline-none focus:ring focus:ring-opacity-25 focus:ring-violet-600 bg-gray-100"
           />
-        </div>
+        </div> */}
         <div>
           <label htmlFor="email" className="block mb-1 ml-1">
             Contact Number *
@@ -45,6 +61,7 @@ const JobApply = () => {
           <input
             id="contact"
             type="tel"
+            name="contact"
             placeholder="+8807176240857"
             required=""
             className="block w-full p-2 rounded focus:outline-none focus:ring focus:ring-opacity-25 focus:ring-violet-600 bg-gray-100"
@@ -52,25 +69,38 @@ const JobApply = () => {
         </div>
         <div>
           <label htmlFor="message" className="block mb-1 ml-1">
-            Description
+            LinkedIn URL
           </label>
-          <textarea
+          <input
             id="message"
-            type="text"
-            placeholder="Your Description..."
-            className="block w-full p-2 rounded autoexpand focus:outline-none focus:ring focus:ring-opacity-25 focus:ring-violet-600 bg-gray-100"
-          ></textarea>
+            type="url"
+            name="linkedin"
+            placeholder="Your LinkedIn URL"
+            className="block w-full p-2 rounded  focus:outline-none focus:ring focus:ring-opacity-25 focus:ring-violet-600 bg-gray-100"
+          ></input>
         </div>
         <div>
           <label htmlFor="message" className="block mb-1 ml-1">
+            GitHub URL
+          </label>
+          <input
+            id="message"
+            type="url"
+            name="github"
+            placeholder="Your GitHub URL"
+            className="block w-full p-2 rounded  focus:outline-none focus:ring focus:ring-opacity-25 focus:ring-violet-600 bg-gray-100"
+          ></input>
+        </div>
+        {/* <div>
+          <label htmlFor="message" className="block mb-1 ml-1">
             Upload Resume
           </label>
-          <input type="file" className="file-input w-full file-input-lg" />
-        </div>
+          <input type="file" name="resume" className="file-input w-full file-input-lg" />
+        </div> */}
         <div>
           <fieldset className="fieldset bg-base-100  border-base-300 rounded-box w-full border p-4">
             <label className="label">
-              <input type="checkbox" defaultChecked className="checkbox" />
+              <input  type="checkbox" defaultChecked className="checkbox" />
               Agree our terms and policy
             </label>
           </fieldset>
