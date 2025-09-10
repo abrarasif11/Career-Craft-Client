0b76508b2abf148e49023bc6f442b4e7b58318a0
abrarasif11 <faasif929@gmail.com>
2025-09-10 19:00:53 +0600

    failed to fetch data axios

--- src/Pages/MyApplication/MyApplication.jsx
+++ src/Pages/MyApplication/MyApplication.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import useAuth from "../../hooks/useAuth";
 import { MdDelete } from "react-icons/md";
 import Swal from "sweetalert2";
@@ -55,20 +55,23 @@ const MyApplication = () => {
       }
     });
 
-  // useEffect(() => {
-  //   if (!user?.email) return;
+    useEffect(() => {
+      if (!user?.email) return;
+    
+      axios
+        .get(`http://localhost:7000/job-application?email=${user.email}`, {
+          withCredentials: true,
+        })
+        .then((res) => setJobs(res.data))
+        .catch((err) => console.error("Axios error:", err));
+    }, [user?.email]);
 
   //   fetch(`http://localhost:7000/job-application?email=${user.email}`)
   //     .then((res) => res.json())
   //     .then((data) => setJobs(data))
   //     .catch((err) => console.error("Fetch error:", err));
-  // }, [user?.email]);
-  axios
-    .get(`http://localhost:7000/job-application?email=${user.email}`, {
-      withCredentials: true,
-    })
-    .then((res) => console.log(setJobs(res.data)));
-
+  //  [user?.email]);
+  
   return (
     <div>
       <h2 className="text-3xl font-semibold text-center mt-10 mb-10">
