26b35b402016e6e37014fe6253ec33feea9f4642
abrarasif11 <faasif929@gmail.com>
2025-09-11 19:56:18 +0600

    fix error in job section

--- src/Pages/SeeAlljobs/SeeAllJobs.jsx
+++ src/Pages/SeeAlljobs/SeeAllJobs.jsx
@@ -20,30 +20,36 @@ const SeeAllJobs = () => {
         >
           {sort ? "Sorted by salary (low → high)" : "Sort by salary"}
         </button>
-        <label className="input flex items-center w-full max-w-2xl mx-auto px-3 py-2 border rounded-lg shadow-sm">
-          <svg
-            className="h-5 w-5 opacity-50 mr-2"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-          >
-            <g
-              strokeLinejoin="round"
-              strokeLinecap="round"
-              strokeWidth="2.5"
-              fill="none"
-              stroke="currentColor"
+        <form
+          onKeyUp={(e) => e.preventDefault()} 
+          className="w-full max-w-2xl mx-auto"
+        >
+          <label className="input w-full flex items-center px-3 py-2 border rounded-lg shadow-sm">
+            <svg
+              className="h-5 w-5 opacity-50 mr-2"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
             >
-              <circle cx="11" cy="11" r="8"></circle>
-              <path d="m21 21-4.3-4.3"></path>
-            </g>
-          </svg>
-          <input
-            onChange={(e) => setSearch(e.target.value)}
-            className="w-full outline-none text-sm sm:text-base"
-            type="text"
-            placeholder="Search Jobs By Location"
-          />
-        </label>
+              <g
+                strokeLinejoin="round"
+                strokeLinecap="round"
+                strokeWidth="2.5"
+                fill="none"
+                stroke="currentColor"
+              >
+                <circle cx="11" cy="11" r="8"></circle>
+                <path d="m21 21-4.3-4.3"></path>
+              </g>
+            </svg>
+            <input
+              type="text"
+              placeholder="Search Jobs By Location"
+              value={search} // ✅ controlled input
+              onChange={(e) => setSearch(e.target.value)}
+              className="w-full outline-none text-sm sm:text-base"
+            />
+          </label>
+        </form>
       </div>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mx-auto">
